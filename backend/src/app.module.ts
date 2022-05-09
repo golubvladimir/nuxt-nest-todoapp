19dd95c603ebb20fbf10fe69fcfae6e0c20c0c4d
Vladimir Golub <golubvlauri@gmail.com>
2022-05-09 11:25:41 +0200

    add typeorm to app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,30 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    // Config module
+    ConfigModule.forRoot({
+      envFilePath: [`${ __dirname }/share/config/.development.env`]
+    }),
+
+    // TypeORM config
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: 'todoapp',
+        entities: [
+          `${ __dirname }/share/entities/*.entity.{.ts,.js}`
+        ]
+      })
+    })
+  ],
 })
 export class AppModule {}
